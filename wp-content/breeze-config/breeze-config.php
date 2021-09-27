a4d584b8132bd3fc788585eaa4cbfdca9cab8db2
whitelotuscorp <contact@whitelotuscorporation.com>
2021-09-27 15:32:08 +0530

    done commit

---
+++ wp-content/breeze-config/breeze-config.php
@@ -0,0 +1,33 @@
+<?php 
+defined( 'ABSPATH' ) || exit;
+return array (
+  'homepage' => 'https://www.whitelotuscorporation.com',
+  'cache_options' => 
+  array (
+    'breeze-active' => '1',
+    'breeze-cross-origin' => '0',
+    'breeze-ttl' => 1440,
+    'breeze-minify-html' => '1',
+    'breeze-minify-css' => '1',
+    'breeze-font-display-swap' => '0',
+    'breeze-minify-js' => '1',
+    'breeze-gzip-compression' => '1',
+    'breeze-browser-cache' => '1',
+    'breeze-desktop-cache' => 1,
+    'breeze-mobile-cache' => 1,
+    'breeze-disable-admin' => '1',
+    'breeze-display-clean' => '1',
+    'breeze-include-inline-js' => '1',
+    'breeze-include-inline-css' => '1',
+  ),
+  'disable_per_adminuser' => '1',
+  'exclude_url' => 
+  array (
+  ),
+  'enabled-lazy-load' => '1',
+  'use-lazy-load-native' => '0',
+  'breeze-preload-links' => '0',
+  'woocommerce_geolocation_ajax' => 0,
+  'permalink_structure' => '/%category%/%postname%/',
+); 
+
---
+++ wp-content/cache/autoptimize/js/autoptimize_snippet_5abf9ab62de03476558e828035f92124.js
@@ -0,0 +1,1 @@
+(function($){"use strict";$(window).on('scroll',function(){var window_top=$(window).scrollTop()+0;if(window_top>0){$('.drdt_sticky_section').addClass('drdt_sticky_fixed');}else{$('.drdt_sticky_section').removeClass('drdt_sticky_fixed');}});}(jQuery));
